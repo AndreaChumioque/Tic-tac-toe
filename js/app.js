663b60b34117859d84b6482c28de9fda84bc20dd
andrea-isabel <andrea19.93.15@gmail.com>
2017-11-09 01:12:42 -0500

    Validando reset, ganador y empate

--- js/app.js
+++ js/app.js
@@ -1,57 +1,69 @@
 window.onload = function () {
   var board = document.querySelector('.board-js');
-  board.addEventListener('click', addPlay);
+  board.addEventListener('click', addMove);
 
   var resetButton = document.querySelector('#reset');
   resetButton.addEventListener('click', resetGame);
 }
 
 var centinel = true;
-var playCount = 0;
+var moves = 0;
 
-function addPlay(event) {
-  console.log(event);
-  if (centinel) {
-    event.target.className = 'play-x';
-    event.target.textContent = 'X';
+function addMove(event) {
+  if (event.target.textContent === '') {
+    if (centinel) {
+      event.target.className = 'play-x';
+      event.target.textContent = 'X';
+    } else {
+      event.target.className = 'play-o';
+      event.target.textContent = 'O';
+    }
 
-  } else {
-    event.target.className = 'play-o';
-    event.target.textContent = 'O';
+    moves++;
+
+    if (moves >= 5 && moves < 10) {
+      if (checkWinner(event.target.textContent)) {
+        document.querySelector('#winnerMsg').textContent = 'Gana el jugador ' + event.target.textContent;
+      } else if(checkWinner(event.target.textContent) == false && moves == 9) {
+        document.querySelector('#winnerMsg').textContent = 'Nadie gana. ¡Es un empate!';      
+      }
+    }
+      
+    centinel = !centinel;
   }
-
-  playCount++;
-  if (playCount >= 5) {
-    checkWinner();
-  }
-  centinel = !centinel;
 }
 
-// var rows = document.querySelector('.board').getElementsByTagName('tr');
-// console.log(rows);
-// console.log(rows[0].children);
+function checkWinner(play) {
+  var centinel = false;
+  if (checkRow(1, 2, 3, play) ||
+    checkRow(4, 5, 6, play) ||
+    checkRow(7, 8, 9, play) ||
+    checkRow(1, 5, 9, play) ||
+    checkRow(3, 5, 7, play) ||
+    checkRow(1, 4, 7, play) ||
+    checkRow(2, 5, 8, play) ||
+    checkRow(3, 6, 9, play)) {
+      centinel = true;
+  }
+  return centinel;
+}
 
-function checkWinner() {
-  var parent = event.target.parentElement.children;
-  console.log(parent.length);
-  // var element = event.target;
-  console.log(parent.index);
-  // if (event.target.isSameNode(event.target.nextSibling) {
+function checkRow(a, b, c, play) {
+  var centinel = false;
+  if (getBox(a) == play && getBox(b) == play && getBox(c) == play)
+    centinel = true;
+  return centinel;
+}
 
-  // }
-  // for (var i = 0; i < parent.length; i++) {
-
-  // }
-
-  // if (parent[i] == event.target) {
-  //   console.log(i);
-  // }
+function getBox(num) {
+  return document.getElementById('s' + num).textContent;
 }
 
 function resetGame(event) {
-  // console.log(event);
-  var cells = document.getElementsByTagName('td');
-  for (var i = 0; i < cells.length; i++) {
-    cells[i].textContent = '';
-  }
+  var boxes = document.getElementsByTagName('td');
+  for (var i = 0; i < boxes.length; i++)
+    boxes[i].textContent = '';
+  
+  document.querySelector('#winnerMsg').textContent = '';
+  moves = 0;
 }
